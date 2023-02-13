a77f17d39f9d818d9e1186d773cbcc0db2bf36a9
yarazarin <89267356+yarazarin@users.noreply.github.com>
2023-02-12 21:53:49 -0500

    added 3 pokemon to scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+//the reference belong: https://pokedex.org/#/pokemon/1 ↓↓↓
+let pokemonList = [
+  {
+    id: 1,
+    name: "Bulbasaur",
+    height: 7,
+    types: ["grass", "poison"],
+    weight: 6.9,
+  },
+
+  { id: 2,
+    name:
+    "Charmander",
+    height: 6,
+    types: ["fire"],
+    weight: 8.5
+  },
+
+  { id: 3,
+    name: "Squirtle",
+    height: 5,
+    types: ["water"],
+    weight: 9 },
+];
+
+//end ↑↑↑
