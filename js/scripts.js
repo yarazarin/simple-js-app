a287189824b7cb30f4f645f41f7816ddd2daedcc
yarazarin <89267356+yarazarin@users.noreply.github.com>
2023-02-13 14:49:08 -0500

    added "for" Loop to pokemonsList and write them to body section

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,41 @@
 //the reference belong: https://pokedex.org/#/pokemon/1 ↓↓↓
 let pokemonList = [
   {
-    1: {
-      name: "Bulbasaur",
-      height: 7,
-      types: ["grass", "poison"],
-      weight: 6.9,
-    },
+    id: 1,
+    name: "Bulbasaur",
+    height: 7,
+    types: ["grass", "poison"],
+    weight: 6.9,
   },
-
-  { 2: { name: "Charmander", height: 6, types: ["fire"], weight: 8.5 } },
-
-  { 3: { name: "Squirtle", height: 5, types: ["water"], weight: 9 } },
+  {
+    id: 2,
+    name: "Charmander",
+    height: 6,
+    types: ["fire"],
+    weight: 8.5,
+  },
+  {
+    id: 3,
+    name: "Squirtle",
+    height: 5,
+    types: ["water"],
+    weight: 9,
+  },
 ];
 
 //end ↑↑↑
+for (let i = 0; i <= pokemonList.length; i++) {
+  // console.log(pokemonList[i].name)
+  document.write(
+    pokemonList[i].id +
+      "-" +
+      pokemonList[i].name +
+      "(" +
+      "weight is : " +
+      pokemonList[i].weight +
+      ") + "
+  );
+  if (pokemonList[i].weight >= 9) {
+    document.write("(❁´◡`) Wow, that's big one!");
+  }
+}
