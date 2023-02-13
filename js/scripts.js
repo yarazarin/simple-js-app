77e46f54e4fee0a43ca598e0933cca535f827274
yarazarin <89267356+yarazarin@users.noreply.github.com>
2023-02-13 10:40:56 -0500

    rewrite id in pokimonList for better readability

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,26 @@
 //the reference belong: https://pokedex.org/#/pokemon/1 ↓↓↓
 let pokemonList = [
   {
-    1: {
-      name: "Bulbasaur",
-      height: 7,
-      types: ["grass", "poison"],
-      weight: 6.9,
-    },
+    id: 1,
+    name: "Bulbasaur",
+    height: 7,
+    types: ["grass", "poison"],
+    weight: 6.9,
   },
-
-  { 2: { name: "Charmander", height: 6, types: ["fire"], weight: 8.5 } },
-
-  { 3: { name: "Squirtle", height: 5, types: ["water"], weight: 9 } },
+  {
+    id: 2,
+    name: "Charmander",
+    height: 6,
+    types: ["fire"],
+    weight: 8.5,
+  },
+  {
+    id: 3,
+    name: "Squirtle",
+    height: 5,
+    types: ["water"],
+    weight: 9,
+  },
 ];
 
 //end ↑↑↑
