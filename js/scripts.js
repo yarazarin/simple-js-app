661f2c216bada3ae197fa71ddfddaeff6329b999
yarazarin <89267356+yarazarin@users.noreply.github.com>
2023-02-16 15:10:54 -0500

    The Pokemon list is update by IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,73 +1,7 @@
 //the reference belong: https://pokedex.org/#/pokemon/1 ↓↓↓
-let pokemonList = [
-  {
-    id: 1,
-    name: "Bulbasaur",
-    height: 7,
-    types: ["grass", "poison"],
-    weight: 6.9,
-  },
-  {
-    id: 2,
-    name: "Charmander",
-    height: 6,
-    types: ["fire"],
-    weight: 8.5,
-  },
-  {
-    id: 3,
-    name: "Squirtle",
-    height: 5,
-    types: ["water"],
-    weight: 9,
-  },
-];
-
-//end ↑↑↑
-
-//write the pokimonList with "for" in body
-// for (let i = 0; i <= pokemonList.length; i++) {
-//   // console.log(pokemonList[i].name)
-//   document.write(
-//     pokemonList[i].id +
-//       "-" +
-//       pokemonList[i].name +
-//       "(" +
-//       "weight is : " +
-//       pokemonList[i].weight +
-//       ") + "
-//   );
-//   if (pokemonList[i].weight >= 9) {
-//     document.write("(❁´◡`) Wow, that's big one!");
-//   }
-// }
-
-// end
-
-/*
-//write the pokimonList with "while" in body
-
-let i = 0;
-while (i <= pokemonList.length) {
-  document.write(
-    pokemonList[i].id +
-      "-" +
-      pokemonList[i].name +
-      "(" +
-      "weight is : " +
-      pokemonList[i].weight +
-      ") + "
-  );
-  if (pokemonList[i].weight >= 9) {
-    document.write("(❁´◡`) Wow, that's big one!");
-  }
-  i++;
-}
-
-// end
-*/
 
 // Update the pokemonList with "forEach"
+/*
 pokemonList.forEach((pokemon) => {
   document.write(
     "<ul> <li>" +
@@ -87,5 +21,78 @@ pokemonList.forEach((pokemon) => {
     );
   }
 });
+*/
 
 // end
+
+const pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      id: 1,
+      name: "Bulbasaur",
+      height: 7,
+      types: ["grass", "poison"],
+      weight: 6.9,
+    },
+    {
+      id: 2,
+      name: "Charmander",
+      height: 6,
+      types: ["fire"],
+      weight: 8.5,
+    },
+    {
+      id: 3,
+      name: "Squirtle",
+      height: 5,
+      types: ["water"],
+      weight: 9,
+    },
+  ];
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  function add(pokemon) {
+    if (typeof pokemon === "object") {
+      pokemonList.push(pokemon);
+    } else {
+      console.error("it is not an object!");
+    }
+  }
+
+  return {
+    getAll: getAll,
+    add: add,
+  };
+})();
+
+pokemonRepository.add({
+  id: 4,
+  name: "New Pokemon",
+  height: 1,
+  types: ["zz"],
+  weight: 1,
+});
+
+let pokemonList = pokemonRepository.getAll();
+pokemonList.forEach((pokemon) => {
+  document.write(
+    "<ul> <li>" +
+      " ID: " +
+      pokemon.id +
+      "-" +
+      pokemon.name +
+      "(" +
+      "weight is : " +
+      pokemon.weight +
+      ")" +
+      "</li> </ul>"
+  );
+  if (pokemon.weight >= 9) {
+    document.write(
+      "<pre class='size__announcer'>" + "that's the big one! ⤴" + "</pre>"
+    );
+  }
+});
